496ea69d4231627bfaadb0e49d8fe9f314d4e6bc
josseandrofarias <farias.josseandro@gmail.com>
2019-08-17 12:41:04 -0400

    Personalicão do navigator implementando material

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,45 +1,66 @@
 import React from "react";
-import {createBottomTabNavigator, createAppContainer} from "react-navigation";
-import Icon from "react-native-vector-icons/FontAwesome";
+import {Text} from "react-native";
+import {createAppContainer} from "react-navigation";
+import {createMaterialBottomTabNavigator} from "react-navigation-material-bottom-tabs";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 import App from "./App";
+import Colaboracoes from "./pages/Colaborar/index";
+import Ajustes from "./pages/Ajustes/index";
 
-const MenuRoutes = createBottomTabNavigator({
-    Feed: {
+Icon.loadFont();
+
+const MenuRoutes = {
+    Mapa: {
         name: 'Mapa',
         screen: App,
         navigationOptions: {
-            title: 'Mapa',
-            tabBarIcon: ({tintColor}) =>
-                <Icon name={'home'} size={30} color={tintColor}/>
-        }
+            // title: 'Mapa',
+            tabBarLabel: <Text style={{color: "white"}}>Mapa</Text>,
+            barStyle: {backgroundColor: "#694fad"},
+            tabBarIcon: ({focused}) =>
+                <Icon name="place" size={25} color={focused ? '#fff' : '#ddd'}/>
+        },
     },
-    Add: {
-        name: 'Colaboracoes',
-        screen: App,
+    Colaboracoes: {
+        name: 'Colaborações',
+        screen: Colaboracoes,
         navigationOptions: {
-            title: 'Colaborações',
+            // title: 'Colaborações',
+            tabBarLabel: <Text style={{color: "white"}}>Colaborações</Text>,
+            barStyle: {backgroundColor: "#03DAC6"},
             tabBarIcon: ({tintColor}) =>
-                <Icon name={'camera'} size={30} color={tintColor}/>
-        }
+                <Icon name={'whatshot'} size={25} color={tintColor}/>
+        },
     },
-    Profile: {
+    Ajustes: {
         name: 'Ajustes',
         screen: App,
         navigationOptions: {
-            title: 'Ajustes',
+            // title: 'Ajustes',
+            tabBarLabel: <Text style={{color: "white"}}>Ajustes</Text>,
+            barStyle: {backgroundColor: "#524365"},
             tabBarIcon: ({tintColor}) =>
-                <Icon name={'user'} size={30} color={tintColor}/>
-        }
-    }
-})
-
-const MenuConfig = {
-    initialRouteName: 'Feed',
-    tabBarOptions: {
-        showLabel: false
+                <Icon name={'menu'} size={25} color={tintColor}/>
+        },
     }
 }
 
-const MenuNavigator = createAppContainer(MenuRoutes, MenuConfig)
-export default MenuNavigator
+const MenuConfig = {
+    // initialRouteName: 'Feed',
+    // tabBarOptions: {
+    //     showLabel: false
+    // }
+    initialRouteName: 'Mapa',
+    activeColor: '#f0edf6', //cor texto
+    inactiveColor: '#3e2465',
+    barStyle: {backgroundColor: '#694fad'},
+    shifting: true, //ocultar texto
+    labeled: true, //mostrar texto quando clicado
+    lazy: true, // todas as guias render
+    tabBarColor: true,
+
+}
+
+const MenuNavigator = createMaterialBottomTabNavigator(MenuRoutes, MenuConfig)
+export default createAppContainer(MenuNavigator)
