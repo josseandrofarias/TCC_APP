47a7199ee9a4f187fcfe24589df2e28d5b6566b5
josseandrofarias <farias.josseandro@gmail.com>
2019-08-18 16:25:40 -0400

    Página de mapa, chamando componente criado

---
+++ src/pages/Mapa/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Map from "../../components/Map/Map";
+export default class Mapa extends Component {
+    render() {
+        return (
+            <Map></Map>
+        );
+    }
+
+};
+
+
+
+
