d443bc3fe7ecb8e3b371a953808824b68aa84853
josseandrofarias <farias.josseandro@gmail.com>
2019-08-22 13:06:22 -0400

    Criação de tela de login com estilização

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {Component, Fragment} from 'react';
 
-const Login = ({ navigation }) => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Login</Text>
-    </View>
-);
+import {View, SafeAreaView, Image, TouchableOpacity, Text, KeyboardAvoidingView, StatusBar} from "react-native";
 
-Login.navigationOptions = {
-    title: 'Login',
+import { Button, TextInput} from 'react-native-paper';
+
+import Styles from'./style'
+
+export default class Login extends Component {
+    state = {
+        login: '',
+        senha: ''
+    };
+
+    render() {
+        return (
+            <View style={Styles.container}>
+
+                <StatusBar
+                    barStyle={"light-content"}
+                />
+
+                <SafeAreaView>
+                    <KeyboardAvoidingView behavior='padding'>
+
+                        <View style={Styles.logoContainer}>
+                            <Image
+                                style={Styles.logo}
+                                source={require('../../imagens/logo.png')}
+                            />
+                        </View>
+
+                        <View style={Styles.inputContainer}>
+                            <TextInput
+                                label='Email'
+                                keyboardType={"email-address"}
+                                returnKeyType={"next"}
+                                mode={'outlined'}
+                                value={this.state.login}
+                                onChangeText={login => this.setState({ login })}
+                                onSubmitEditing={() => this.passwordInput.focus()}
+                                autoCapitalize={"none"}
+                                autoCorrect={false}
+                                style={Styles.input}
+                            />
+                            <TextInput
+                                label='Senha'
+                                returnKeyType={"go"}
+                                mode={'outlined'}
+                                value={this.state.senha}
+                                secureTextEntry={true}
+                                onChangeText={senha => this.setState({ senha })}
+                                style={Styles.input}
+                                ref={(input) => this.passwordInput = input}
+                            />
+                        </View>
+
+                        <TouchableOpacity style={Styles.buttonEsqueciContainer}>
+                            <Text style={Styles.buttonText}>
+                                Esqueci minha senha
+                            </Text>
+                        </TouchableOpacity>
+
+                        <View style={Styles.buttonContainer}>
+                            <Button
+                                color={"#694fad"}
+                                icon=""
+                                mode="contained"
+                                onPress={() => console.log('Pressed')}
+                                style={Styles.button}
+                            >
+                                Entrar
+                            </Button>
+                            <Button
+                                color={"#7672d1"}
+                                icon=""
+                                mode="contained"
+                                onPress={() => console.log('Pressed')}
+                                style={Styles.button}
+                            >
+                                Criar uma Conta
+                            </Button>
+                        </View>
+                    </KeyboardAvoidingView>
+                </SafeAreaView>
+            </View>
+        );
+    }
 }
-
-export default Login;
--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -1,41 +1,43 @@
-import {StyleSheet} from "react-native";
-import {Colors} from "react-native/Libraries/NewAppScreen";
+import { StyleSheet } from 'react-native';
+// "#694fad"
+const styles =  StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#03a18d",
+    },
+    logoContainer:{
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center',
+        marginTop: '10%'
+    },
+    logo:{
+        width: 150,
+        height: 150,
+    },
+    buttonLogin:{
+        backgroundColor: "#694fad"
+    },
+    input: {
+        marginBottom: 20,
+        paddingHorizontal: 10,
+    },
+    inputContainer: {
+        padding: 20
+    },
+    button: {
+        marginBottom: 20,
+        paddingHorizontal: 10,
+    },
+    buttonContainer: {
+        padding: 40
+    },
+    buttonEsqueciContainer: {
+        padding: 20
+    },
+    buttonText: {
+        color: "rgba(123,86,208,0.99)"
+    },
+})
 
-const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    engine: {
-        position: 'absolute',
-        right: 0,
-    },
-    body: {
-        backgroundColor: Colors.white,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-});
+export default styles
