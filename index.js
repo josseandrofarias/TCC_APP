b93ccbf7cb35f033cbc9630828d814690b452143
josseandrofarias <farias.josseandro@gmail.com>
2019-09-01 18:21:24 -0400

    Alteração de page principal

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
  */
 import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import Login from "./src/pages/Login/index"
 import { Provider as PaperProvider } from 'react-native-paper';
 import {name as appName} from './app.json';
 import { YellowBox } from 'react-native';
+import Ajustes from './src/pages/Ajustes';
+import App from './src/App';
 
 YellowBox.ignoreWarnings([
     'Warning: componentWillMount is deprecated',
